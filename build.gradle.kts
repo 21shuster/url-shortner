3761a5ad35d421e55225fb00a5b8403b74d6a78c
Alejandro Garcia <alejandrogarcia@MacBook-Pro-de-Alejandro.local>
2025-11-03 11:28:17 +0100

    Add base dependencies for Spring Boot, MongoDB, and Kotlin configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,31 @@
 plugins {
     kotlin("jvm") version "2.2.20"
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.5"
+    kotlin("plugin.spring") version "2.0.20"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+
     testImplementation(kotlin("test"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 }
 
 tasks.test {
